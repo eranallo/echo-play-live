f87c7d3dcbbaa62b0089ce35e718427fc93d0bac
eranallo <eranallo@echoplay.live>
2026-04-09 13:25:34 -0500

    Add files via upload

--- epl-app/pages/_app.js
+++ epl-app/pages/_app.js
@@ -5,15 +5,12 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Barlow:wght@300;400;500;600;700&family=Barlow+Condensed:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" href="/logo.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Echo Play Live" />
-        <meta name="theme-color" content="#080808" />
+        <meta name="theme-color" content="#0a0a0f" />
       </Head>
       <Component {...pageProps} />
     </>
